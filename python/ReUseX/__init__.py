b3fa0cc2c536f3228fbb0f955c4c071848081cfe
Povl Filip Sonne-Frederiksen <22501790+pfmephisto@users.noreply.github.com>
2025-02-28 09:00:59 +0100

    Updating logging in __init__ file

--- python/ReUseX/__init__.py
+++ python/ReUseX/__init__.py
@@ -1,33 +1,42 @@
 from __future__ import annotations
 
-import logging
+#import logging
 from os import getpid
+
+import spdlog
+logger = spdlog.ConsoleLogger("ReUseX")
+logger.set_level(spdlog.LogLevel.DEBUG)
+
+from ._core import *
+from ._core import __doc__, __version__
+
+_core.register_logger(logger.get_underlying_logger())
+#logger.set_pattern("[%l] %n: %v")
+# register logger with the function from mylib, so it is accessible inside MyClass
+
 
 # # Define the logger format
 # logging.basicConfig(level=logging.INFO, format=log_format)
 
-#only enable logging for this module
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+##only enable logging for this module
+#logger = logging.getLogger(__name__)
+#logger.setLevel(logging.DEBUG)
+#
+## Disable propagation to prevent duplicate messages from root handlers
+#logger.propagate = False
+#
+#console_logger = logging.StreamHandler()
+#console_logger.setLevel(logging.DEBUG)
+#console_logger.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+#logger.addHandler(console_logger)
+#
+## Ensure no duplicate handlers are added
+#if not logger.hasHandlers():
+#    logger.addHandler(console_logger)
 
-# Disable propagation to prevent duplicate messages from root handlers
-logger.propagate = False
-
-console_logger = logging.StreamHandler()
-console_logger.setLevel(logging.DEBUG)
-console_logger.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-logger.addHandler(console_logger)
-
-# Ensure no duplicate handlers are added
-if not logger.hasHandlers():
-    logger.addHandler(console_logger)
 
 logger.debug("Thread ID: %s", getpid())
 logger.debug("Loading ReUseX")
-
-
-from ._core import *
-from ._core import __doc__, __version__
 
 
 from .cli import run, ml
